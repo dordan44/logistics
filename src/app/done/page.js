6ab3fb0eed9b95c9c1e3f357f10a335590641ffa
Dor Dadon <dordan44@gmail.com>
2023-11-04 21:14:23 +0200

    Splitting requests screen to pending and done by a different page

--- src/app/done/page.js
+++ src/app/done/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-    fetch("https://api.bandwatch.co.il/requests")
+    fetch("https://api.bandwatch.co.il/done")
       .then((res) => res.json())
       .then((data) => {
         console.log(data)
@@ -19,7 +19,7 @@ export default function Home() {
   return (
     <main>
       <Image className="main-image" src={"/shalishut.png"} width={80} height={80} alt="Shalishut tag"/>
-      <h1>מערך החמ״לים</h1>
+      <h1>מערך החמ״לים - הושלם</h1>
       {isLoading && <div className="loader">טוען...</div>}
       {items && items.map(item => <Request request={item} key={item._id} />)}
     </main>
