eff2cb19a7f74ede709d2656530f0116ba8a4cff
Dor Dadon <dordan44@gmail.com>
2023-11-04 19:59:22 +0200

    fixing next.config

--- _static/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/dordadon/Desktop/Full stack/war-room-logistics/src/app/layout\":[\"static/media/25e11f223a13018e-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- _static/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"KQldhA3ZRPCYpQeVbGc3au++SZ6rCH+szDj4GprkShU=\"\n}"
